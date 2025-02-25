dac8afa62f8e6f577887ab1209619c7d1cce3634
gcarpen480 <gcarpen480@g.educaand.es>
2025-02-25 22:00:58 +0100

    Subiendo Ejercicio Base Examen

---
+++ ejercicio1_examen/src/main/java/ies/castillodeluna/psp/ejercicio1_examen/Ejercicio1ExamenApplication.java
@@ -0,0 +1,13 @@
+package ies.castillodeluna.psp.ejercicio1_examen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ejercicio1ExamenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ejercicio1ExamenApplication.class, args);
+	}
+
+}
---
+++ ejercicio1_examen/src/test/java/ies/castillodeluna/psp/ejercicio1_examen/Ejercicio1ExamenApplicationTests.java
@@ -0,0 +1,13 @@
+package ies.castillodeluna.psp.ejercicio1_examen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ejercicio1ExamenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
